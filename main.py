7edbfcc70c27485a6e28cb70547ccb8048503207
elanovviktor <elanov_viktor@mail.ru>
2024-09-04 19:15:28 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+data_structure = [
+  [1, 2, 3],
+  {'a': 4, 'b': 5},
+  (6, {'cube': 7, 'drum': 8}),
+  "Hello",
+  ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+def calculate_structure_sum(*args):
+    total_sum = 0
+    for arg in args:
+        if isinstance(arg, (int,float)):
+            total_sum += arg
+        elif isinstance(arg, str):
+            total_sum += len(arg)
+        elif isinstance(arg, (list, tuple, set)):
+            total_sum += calculate_structure_sum(*arg)
+        elif isinstance(arg, dict):
+            total_sum += calculate_structure_sum(*arg.items())
+    return total_sum
+
+result = calculate_structure_sum(data_structure)
+print(result)
+
